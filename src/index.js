6743083b80ee22d083b285e8d2519b51e17afe88
Thomas Rayden <thomasmwrayden@gmail.com>
2019-11-26 17:52:40 +0800

    Add more navigation methods

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import bowser from "bowser";
+const bowser = require("bowser");
 
-function armFallbackTimer(callback) {
+function createHiddenFrame() {
+  const frame = document.createElement("iframe");
+  frame.style =
+    "position: fixed;top: 0;left: 0;right: 0;bottom: 0;visibility: hidden;";
+  return frame;
+}
+
+function blurFallbackTimer(callback) {
   /**
    * If the window hasn't lost focus in 5000ms
    * we know that the deep link has failed and we can call the fallback
@@ -14,21 +21,56 @@ function armFallbackTimer(callback) {
 
 const methods = {
   universal(uri, callback) {
-    armFallbackTimer(callback);
+    /**
+     * Chrome and others technique
+     */
+    blurFallbackTimer(callback);
     window.location.replace(uri);
   },
-  iframe(uri, callback) {
-    armFallbackTimer(callback);
+  frame(uri, callback) {
     /**
+     * Safari technique
+     *
      * Safari needs to be tricked with an iframe
      */
-    const trick = document.createElement("iframe");
-    trick.style =
-      "position: fixed;top: 0;left: 0;right: 0;bottom: 0;visibility: hidden;";
-    trick.src = uri;
-    document.body.appendChild(trick);
+    blurFallbackTimer(callback);
+    const frame = createHiddenFrame();
+    frame.src = uri;
+    document.body.appendChild(frame);
+  },
+  frameWCatch(uri, callback) {
+    /**
+     * Firefox technique
+     */
+    const frame = createHiddenFrame();
+    document.body.appendChild(frame);
+    try {
+      frame.contentWindow.location.href = uri;
+    } catch (err) {
+      console.error(err);
+      callback();
+    }
+  },
+  frameWCORS(uri, callback) {
+    /**
+     * Opera technique
+     */
+    const frame = createHiddenFrame();
+    frame.src = uri;
+    document.body.appendChild(frame);
+    setTimeout(function() {
+      try {
+        console.log(frame.contentWindow.location);
+      } catch (err) {
+        console.error(err);
+        callback();
+      }
+    }, 0);
   },
   msLaunch(uri, callback) {
+    /**
+     * Microsoft Windows technique
+     */
     window.navigator.msLaunchUri(uri, function() {}, callback);
   }
 };
@@ -41,23 +83,30 @@ export function openNativeLink(uri, fallback) {
     throw new TypeError("fallback must be of type function");
   }
   const browser = bowser.getParser(window.navigator.userAgent);
-  const useFrame = browser.satisfies({
-    macos: {
-      safari: ">11"
-    },
-    firefox: "*"
-  });
-  const useMsLaunch =
-    browser.satisfies({
-      windows: {
-        ie: "*",
-        edge: "*"
+  const useFrame = browser.some([
+    {
+      desktop: {
+        safari: "*"
       }
-    }) &&
-    typeof window.navigator.msLaunchUri === "function" &&
-    uri.indexOf("://") !== -1;
+    }
+  ]);
+  const useFrameWCatch = browser.some([{ desktop: { firefox: "*" } }]);
+  const useFrameWCORS = browser.some([{ desktop: { opera: "*" } }]);
+  const useMsLaunch =
+    browser.some([
+      {
+        desktop: {
+          ie: "*"
+        }
+      },
+      { desktop: { edge: "*" } }
+    ]) && typeof window.navigator.msLaunchUri === "function";
   if (useFrame) {
-    methods.iframe(uri, fallback);
+    methods.frame(uri, fallback);
+  } else if (useFrameWCatch) {
+    methods.frameWCatch(uri, fallback);
+  } else if (useFrameWCORS) {
+    methods.frameWCORS(uri, fallback);
   } else if (useMsLaunch) {
     methods.msLaunch(uri, fallback);
   } else {
